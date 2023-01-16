0a57a143f4f53915f5794fa624a16bc543f63bd0
DICCE <heback@gmail.com>
2023-01-16 14:10:36 +0900

    2023-01-16 #2

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ st.subheader('회원가입 폼')
 with st.form('my_form', clear_on_submit=True):
     st.info('다음 양식을 모두 작성한 다음 제출합니다.')
     uid = st.text_input('아이디', max_chars=12)
+
+    submitted = st.form_submit_button('제출')
+    if submitted:
+        st.success('제출됨')
