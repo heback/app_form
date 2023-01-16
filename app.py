de46134546440db00338327cd86646823d70446a
DICCE <heback@gmail.com>
2023-01-16 14:06:39 +0900

    2023-01-16 #2

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ import streamlit as st
 st.subheader('회원가입 폼')
 
 with st.form('my_form', clear_on_submit=True):
-    st.success('다음 양식을 모두 작성한 다음 제출합니다.')
+    st.info('다음 양식을 모두 작성한 다음 제출합니다.')
     uid = st.text_input('아이디', max_chars=12)
